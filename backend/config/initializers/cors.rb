ae13cb07300da24de32b43caed23231d6265d079
daidtech <37820071+daidtech@users.noreply.github.com>
2026-04-18 09:49:36 +0700

    quick fix cors issue

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'demo1.dinhvandai.com' # Replace '*' with specific domains in production; for testing, keep it as '*' for easier testing
+    origins '*' # Replace '*' with specific domains in production; for testing, keep it as '*' for easier testing
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
