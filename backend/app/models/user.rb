8f4897cdd4ec1e81a3c2a45029a043c992693ba0
daidtech <37820071+daidtech@users.noreply.github.com>
2026-04-20 06:55:41 +0700

    refactor: remove unused devise modules and skip_confirmation method from User model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,15 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable, and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+  devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
   has_many :videos, dependent: :destroy
-
-  private
-
-  def skip_confirmation
-    self.skip_confirmation!
-  end
-
 end
